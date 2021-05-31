1b4bf3acc3422aaaa6463858bea8c5a6f1a104e3
funkyxive <andreasnordenhof@gmail.com>
2021-05-31 12:23:43 +0200

    fixed some of the moddels and added string methods

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(address)
+admin.site.register(Group)
+admin.site.register(CustomFeed)
+admin.site.register(Profile)
+admin.site.register(Event)
+admin.site.register(Post)
+admin.site.register(PostComment)
+admin.site.register(ReplyComment)
+
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,55 +1,69 @@
 from typing import Callable
 from django.db import models
-from django.db.models import CharField, TextField, ManyToManyField, OneToOneField, ForeignKey, DateField, TimeField, DateTimeField, IntegerField, DecimalField, CASCADE,
+from django.db.models import CharField, TextField, ManyToManyField, OneToOneField, ForeignKey, DateField, TimeField, DateTimeField, IntegerField, DecimalField, CASCADE
 import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.conf import settings
 # Create your models here.
-class address(models.model):
-    profileAddress = CharField(max_length=100)
-    profileCity = CharField(max_length=50)
-    profilePostalCode = IntegerField()
-
+class address(models.Model):
+    address = CharField(max_length=100)
+    city = CharField(max_length=50)
+    postalCode = IntegerField()
+    def __str__(self):
+        return f"{self.address} {self.city} {self.postalCode}"
 class Group(models.Model):
+    name = CharField(max_length=50)
     description = TextField()
-    owner = ForeignKey(User, on_delete=CASCADE)
+    owner = ForeignKey(User, on_delete=CASCADE, related_name="groupOwner")
     members = ManyToManyField(User)
+    def __str__(self):
+        return f"{self.name}"
 
 class CustomFeed(models.Model):
+    name = CharField(max_length=50)
     user_source = ForeignKey(User, on_delete=CASCADE)
     group_source = ForeignKey(Group, on_delete=CASCADE)
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Profile(models.Model):
     profileUser = OneToOneField(User, on_delete=CASCADE)
     profilePhoneNumber = DecimalField(decimal_places=0, max_digits=8)
     profileAddress = ForeignKey(address, on_delete=CASCADE)
-    friendlist = ManyToManyField(User)
+    friendlist = ManyToManyField(User, related_name="userFriendlist")
     customFeeds = ManyToManyField(CustomFeed)
     def __str__(self):
         return f"{self.profileUser}"
 
 class Event(models.Model):
-    participants = ManyToManyField (User)
-    host = ForeignKey(User, on_delete=CASCADE)
+    name = CharField(max_length=50)
+    participants = ManyToManyField (User, related_name="participatingUsers")
+    host = ForeignKey(User, on_delete=CASCADE, related_name="hostingUser")
     start_datetime = DateTimeField()
     end_datetime = DateTimeField()
     address = ForeignKey(address, on_delete=CASCADE)
     description = TextField()
+    def __str__(self):
+        return f"{self.name}"
 
 class Post(models.Model):
     author = ForeignKey(User, on_delete=CASCADE)
     content = TextField()
     score = IntegerField()
+    def __str__(self):
+        return f"{self.author}'s post"
 
-class Comment(models.Model):
+class PostComment(models.Model):
     content = TextField()
-    author = ForeignKey(User)
+    author = ForeignKey(User, on_delete=CASCADE)
     score = IntegerField()
-
-class PostComment(Comment):
     parent_post = ForeignKey(Post, on_delete=CASCADE)
-class CommentComment(Comment):
-    parent_comment = ForeignKey(Comment, on_delete=CASCADE)
+
+class ReplyComment(models.Model):
+    content = TextField()
+    author = ForeignKey(User, on_delete=CASCADE)
+    score = IntegerField()
+    parent_comment = ForeignKey(PostComment, on_delete=CASCADE, related_name="commentParent")
